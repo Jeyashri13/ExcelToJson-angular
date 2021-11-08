3379c8b5347997a7df2c9ee0aac0ef17e03225e2
Jeyashri13 <jeyasiva774@gmail.com>
2021-11-08 14:11:43 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'excel-json';
+convertedJson! : string;
+  fileUpload(event:any){
+    console.log(event.target.files);
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) => {
+      console.log(event);
+      let binaryData = event.target?.result;
+      let workbook = XLSX.read(binaryData, {type: 'binary'});
+      workbook.SheetNames.forEach(sheet => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        console.log(data);
+        this.convertedJson = JSON.stringify(data, undefined,4)
+      })
+      console.log(workbook);
+    }
+  }
 }
